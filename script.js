4fc70058a4ea6bc3758c1b73c30a395c25102ea8
Gurnav224 <chaudharyg856@gmail.com>
2022-07-21 10:56:22 +0530

    Team-Player

---
+++ script.js
@@ -0,0 +1,74 @@
+
+
+// Function created  for Player name update 
+
+// updateName() is for update Name of player 1 Name
+//Team_csk function
+function updateName(){
+    const para = document.querySelector('#player1');
+
+    para.addEventListener('click',updateName);
+
+        const name = prompt('enter a new name');
+        para.textContent = `Player 1: ${name}`
+
+}
+
+//updateName() is for update Name of player 2 Name
+function updateName2(){
+    const para = document.querySelector('#player2');
+
+    para.addEventListener('click',updateName2);
+
+        const name = prompt('enter a new name');
+        para.textContent = `Player 2: ${name}`
+
+}
+
+//updateName() is for update Name of player 3 Name
+function updateName3(){
+    const para = document.querySelector('#player3');
+
+    para.addEventListener('click',updateName3);
+
+        const name = prompt('enter a new name');
+        para.textContent = `Player 3: ${name}`
+
+}
+
+
+
+
+//team_mi function started here
+//updateName() is for update Name of player 1 Name
+function updateName4(){
+    const para = document.querySelector('#player4');
+
+    para.addEventListener('click',updateName);
+
+        const name = prompt('enter a new name');
+        para.textContent = `Player 1: ${name}`
+
+}
+
+//updateName() is for update Name of player 2 Name
+function updateName5(){
+    const para = document.querySelector('#player5');
+
+    para.addEventListener('click',updateName2);
+
+        const name = prompt('enter a new name');
+        para.textContent = `Player 2: ${name}`
+
+}
+
+//updateName() is for update Name of player 3 Name
+function updateName6(){
+    const para = document.querySelector('#player6');
+
+    para.addEventListener('click',updateName3);
+
+        const name = prompt('enter a new name');
+        para.textContent = `Player 3: ${name}`
+
+}
